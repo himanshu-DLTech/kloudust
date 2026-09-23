30a4b5bb3fa113db93aba8b6ef9d91815d957cdf
himanshuDLTech <himanshu.gupta@deeplogictech.com>
2026-09-23 09:52:15 +0000

    [update] assignIPToVM & assignVnetIP command to use the rediness check command

--- backend/apps/kloudust/lib/cmd/assignVnetIP.js
+++ backend/apps/kloudust/lib/cmd/assignVnetIP.js
@@ -12,6 +12,7 @@ const roleman = require(`${KLOUD_CONSTANTS.LIBDIR}/roleenforcer.js`);
 const createVM = require(`${KLOUD_CONSTANTS.LIBDIR}/cmd/createVM.js`);
 const createVnet = require(`${KLOUD_CONSTANTS.LIBDIR}/cmd/createVnet.js`);
 const dbAbstractor = require(`${KLOUD_CONSTANTS.LIBDIR}/dbAbstractor.js`);
+const getVMReadiness = require(`${KLOUD_CONSTANTS.LIBDIR}/cmd/getVMReadiness.js`);
 const {xforge} = require(`${KLOUD_CONSTANTS.THIRD_PARTY_DIR}/xforge/xforge`);
 const CMD_CONSTANTS = require(`${KLOUD_CONSTANTS.LIBDIR}/cmd/cmdconstants.js`);
 
@@ -45,22 +46,9 @@ module.exports.exec = async function(params) {
     const hostInfoVM = await dbAbstractor.getHostEntry(vm.hostname); 
     if (!hostInfoVM) {params.consoleHandlers.LOGERROR("Bad hostname for the VM or host not found"); return CMD_CONSTANTS.FALSE_RESULT();}
 
-    //check if the guest agent is running before making any changes
-    const xforgeArgsGuestCheck = {
-        colors: KLOUD_CONSTANTS.COLORED_OUT, 
-        file: `${KLOUD_CONSTANTS.THIRD_PARTY_DIR}/xforge/samples/remoteCmd.xf.js`,
-        console: params.consoleHandlers,
-        other: [
-            hostInfoVM.hostaddress, hostInfoVM.rootid, hostInfoVM.rootpw, hostInfoVM.hostkey, hostInfoVM.port,
-            `${KLOUD_CONSTANTS.LIBDIR}/cmd/scripts/guestCheck.sh`,
-            vm.name, KLOUD_CONSTANTS.CONF.MAX_GUEST_AGENT_CHECK_WAIT
-        ]
-    }
-
-    let guestCheck = await xforge(xforgeArgsGuestCheck);
-
-    if (!guestCheck.result) {
-        params.consoleHandlers.LOGERROR(`QEMU Guest agent is not running inside the vm ${vm.name}`); 
+    const readiness = await getVMReadiness.checkReadiness(vm.name, hostInfoVM, params.consoleHandlers);
+    if (!readiness.status) {
+        params.consoleHandlers.LOGERROR(`VM ${vm.name_raw} is not ready for guest-agent-dependent operations`);
         return CMD_CONSTANTS.FALSE_RESULT();
     }
 
